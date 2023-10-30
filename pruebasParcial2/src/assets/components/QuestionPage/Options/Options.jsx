4b6ed3c62fa9fb056df842dfe4ae477d80a3a1ba
diegocalvo05 <00103322@uca.edu.sv>
2023-10-30 13:35:46 -0600

    interactividad en botones de respuestas

--- pruebasParcial2/src/assets/components/QuestionPage/Options/Options.jsx
+++ pruebasParcial2/src/assets/components/QuestionPage/Options/Options.jsx
@@ -1,19 +1,41 @@
 
-
 const Options = ({data={}}) => {
+  
+  const shuffle = (array=[]) => { //genera un orden aleatorio en un array
+    return array.sort(() => Math.random() - 0.5); 
+  }; 
+  
+  const answers = [...data.incorrect_answers, data.correct_answer];
+  const shuffledAnswers = shuffle(answers);
+  let canChoose = true;
+
+  const onClickHandler = (e) => {
+
+    if(canChoose){
+      if(e.target.value === data.correct_answer){
+        e.target.style.backgroundColor = "rgb(163 230 53)";
+        canChoose = !canChoose;
+      } else {
+        e.target.style.backgroundColor = "rgb(239 68 68)";
+        canChoose = !canChoose;
+      }
+    }
+  }
+
 
   return (
     <section className="flex justify-center items-center">
-      <div className="text-black w-64 h-24 bg-white rounded-lg flex justify-center items-center m-4 hover:cursor-pointer">
-        {data.correct_answer}
-      </div>
-
       {
-        data.incorrect_answers.map((r) => {
+        shuffledAnswers.map((r) => {
           return (
-            <div key={Math.random} className="text-black w-64 h-24 bg-white rounded-lg flex justify-center items-center m-4 hover:cursor-pointer">
+            <button 
+              key={Math.random} 
+              className="text-black w-64 h-24 bg-white rounded-lg flex justify-center items-center m-4 hover:cursor-pointer hover:bg-cyan-300"
+              value={r}
+              onClick={onClickHandler}
+            >
               {r}
-            </div>
+            </button>
           )
         })
       }
